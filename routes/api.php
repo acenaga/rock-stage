1c341610ad34638da89037e3a76898a91c45b1cd
Prof. Acenaga <carlos.ferrer@davinci.edu.ar>
2026-01-30 21:21:40 -0300

    fixed videos.index route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\VideoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->name('v1.')->group(function () {
     Route::apiResource('videos', VideoController::class);
 });
 
